bdfdcc08663cf705b78524d5d5793443db8eb027
Szymon Lacz <43669678+Beas00@users.noreply.github.com>
2022-01-07 22:19:03 +0000

    Initialize

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import classNames from "classnames";
+
+import styles from "./App.module.css";
+import useMediaQuery from "./hooks/useMediaQuery";
+
+
+const App = () => {
+  const isMobile = useMediaQuery(768);
+  const isTablet = useMediaQuery(1024);
+  return (
+    <div
+      className={classNames([
+        styles.layout,
+        isMobile && styles.mobile,
+        isTablet && styles.tablet,
+      ])}
+      
+    >
+      <h1>{isMobile ? "Mobile" : isTablet ? "Tablet" : "Desktop"}</h1>
+    </div>
+  );
+};
+
+export default App;
